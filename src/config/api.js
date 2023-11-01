24826e09a16ff701e56166e3636eba3cbca901ac
José Rafael <rafaelmatiashue@gmail.com>
2023-11-01 20:06:22 -0300

    tete

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
- baseURL: 'http://localhost:3333/'
+ baseURL: 'https://api-estrela.onrender.com'
   //BASE DA API ON-LINE baseURL: 'https://api-neobet.onrender.com'
   //BASE PARA RODAR LOCALMENTE baseURL: 'http://localhost:3333/'   
 }); 
